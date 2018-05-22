20bdd414a45806d9881d259bdaa21a9e500f2773
Bryan Concari <bpalmer@mcclatchy.com>
2018-05-22 10:51:42 -0400

    Updating dnsscan.sh to only collect zone files if $ZONEDIR does not exist

--- dnsscan.sh
+++ dnsscan.sh
@@ -2,16 +2,33 @@
 
 # Run this script on a named slave or master
 
+# If zones/ is a directory, and contains one or more files,
+# then the script will _just_ parse the zone files to $OUTPUT.
+
+# If zones/ is not a directory, or doesn't exist, the script
+# will create it and populate it with zone files from $ZONEDIR.
+
 ZONEDIR=/var/named/slaves
 OUTPUT=totality.txt
 
-[ ! -d zones ] && mkdir zones
-[ -f $OUTPUT ] && rm $OUTPUT
-if [ ! -d $ZONEDIR ]; then
-    echo "Error: $ZONEDIR does not exist" >&2
-    exit 1;
+NO_COLLECT=0
+
+if [ -d zones ]; then
+    if [ $(ls -1 zones/ | wc -l) -gt 0 ]; then
+        NO_COLLECT=1
+    fi
 fi
 
-find $ZONEDIR -type f -print | xargs -I% sh -c "echo %; if [ \"\$(./parse_bin.rb %)\" ]; then named-compilezone -f raw -F text -o zones/\$(basename %.txt) \$(./parse_bin.rb %) % ; fi ; echo"
+if [ $NO_COLLECT -eq 0 ]; then
+    if [ ! -d $ZONEDIR ]; then
+        echo "Error: $ZONEDIR does not exist" >&2
+        exit 1;
+    fi
+    mkdir zones
+fi
+
+[ -f $OUTPUT ] && rm $OUTPUT
+
+[ $NO_COLLECT -eq 0 ] && find $ZONEDIR -type f -print | xargs -I% sh -c "echo %; if [ \"\$(./parse_bin.rb %)\" ]; then named-compilezone -f raw -F text -o zones/\$(basename %.txt) \$(./parse_bin.rb %) % ; fi ; echo"
 for zonefile in zones/*; do ./parse_zone.rb $zonefile >> $OUTPUT; done
 
